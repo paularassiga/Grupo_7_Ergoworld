fbbfe46eca2618309dc17bd0aa1d0647a5483db8
Peters0n <petersonflemm@gmail.com>
2021-02-15 12:04:51 -0400

    Partial head

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -11,7 +11,7 @@ let mainControllers = {
         res.render("index")
     },
     login: (req,res) => {
-        res.render("login")
+        res.render("user/login")
 
     },
     register: (req,res) => {
