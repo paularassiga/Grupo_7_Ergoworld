8a384bf94a0a9506f3f34f7b8432c83255ce7608
Guillermo Rodriguez <48388462+guille-developer@users.noreply.github.com>
2021-03-05 21:52:11 -0300

    vvalidacion basica creacion producto

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -1,7 +1,10 @@
 const path = require('path'); /*Para usar path.join*/
 const fs = require("fs");
 const products = require("../data/productModel.js");
-
+const {
+  validationResult
+} = require("express-validator");
+const { report } = require('../routers/productoRoutes.js');
 
 
 /*Funciones*/
@@ -18,35 +21,43 @@ let productoControllers = {
     res.render('products/product-create-form');
   },
 
-  store:  (req, res) => {
- console.log(req.body);
-    let productos = [];
-    
-    let nuevoProducto = {
+  store: (req, res) => {
+    console.log(req.body);
 
-      id: 1,
+    let errors = validationResult(req)
 
 
 
-      ...req.body
-    }
-    productos.push(nuevoProducto);
-    console.log(productos);
-    let stringProducto = JSON.stringify(productos);
-     fs.writeFileSync(path.join(__dirname, '../data/productos.json'), stringProducto, function (result, error) {
-      if (error) {
-        res.send(error)
-      }else{
-        res.render('products/product-create-form', {mensage: "El producto ha sido creado correctamente"});
+
+    if(errors.isEmpty()){
+      let productos = [];
+
+      let nuevoProducto = {
+        id: 1,
+        ...req.body
       }
-    });
+      productos.push(nuevoProducto);
+      console.log(productos);
+      let stringProducto = JSON.stringify(productos);
+      fs.writeFileSync(path.join(__dirname, '../data/productos.json'), stringProducto, function (result, error) {
+        if (error) {
+          res.send(error)
+        } else {
+          res.render('products/product-create-form', {
+            mensage: "El producto ha sido creado correctamente"
+          });
+        }
+      });
+      // res.send(nuevoProducto)
+      
+    }else{
 
 
-    // res.send(nuevoProducto)
-    res.render('products/product-create-form');
+      res.render('products/product-create-form', {oldData: req.body, errors: errors.mapped()});
+
+    }
+    
   },
-
-
   edit: async (req, res) => {
 
 
--- routers/productoRoutes.js
+++ routers/productoRoutes.js
@@ -1,16 +1,46 @@
 const express = require('express');
-const multer = require("multer")
-const path = require("path")
+const multer = require("multer");
+const path = require("path");
+const {check} = require("express-validator");
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
-        cb(null, './public/images/')
+        cb(null, './public/images/');
     },
     filename: function(req, file, cb){
         cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`);
     }
 });
 
-const uploadFile = multer({storage})
+const validateCrearProducto = [
+  check('name').notEmpty().withMessage('Debes ingresar un nombre para el producto'),
+  check('short-description').notEmpty().withMessage('Debes ingresar una descripcion corta'),
+  check('price').notEmpty().isInt().withMessage("Debes ingresar un precio valido"),
+  check('characteristic-1').notEmpty().withMessage('Debes ingresar una caracteritica valida'),
+  check('characteristic-2').notEmpty().withMessage('Debes ingresar una caracteritica valida'),
+  check('characteristic-3').notEmpty().withMessage('Debes ingresar una caracteritica valida'),
+  check('characteristic-4').notEmpty().withMessage('Debes ingresar una caracteritica valida'),
+  check('title-description-1').notEmpty().withMessage('Debes ingresar una titulo valido'),
+  check('description-1').notEmpty().withMessage('Debes ingresar una descripcion validoa'),
+  check('title-description-2').notEmpty().withMessage('Debes ingresar una titulo valido'),
+  check('description-2').notEmpty().withMessage('Debes ingresar una descripcion validoa'),
+  check('title-description-3').notEmpty().withMessage('Debes ingresar una titulo valido'),
+  check('description-3').notEmpty().withMessage('Debes ingresar una descripcion validoa'),
+  check('title-description-4').notEmpty().withMessage('Debes ingresar una titulo valido'),
+  check('description-4').notEmpty().withMessage('Debes ingresar una descripcion validoa'),
+  check('image-1').notEmpty().withMessage("Debes subir una imagen"),
+  check('image-2').notEmpty().withMessage("Debes subir una imagen"),
+  check('image-3').notEmpty().withMessage("Debes subir una imagen"),
+  check('image-4').notEmpty().withMessage("Debes subir una imagen"),
+  check('category').notEmpty().withMessage("Debes Elegir al menos una categoria"),
+
+]
+
+
+
+
+
+
+const uploadFile = multer({storage});
 
 
  const productoControllers = require('../controllers/productoController.js');
@@ -29,7 +59,7 @@ const uploadFile = multer({storage})
 
    /*CREAR PRODUCTO*/
  router.get('/crear', productoControllers.create);
- router.post('/crear', uploadFile.any(),  productoControllers.store)
+ router.post('/crear', uploadFile.any(), validateCrearProducto,  productoControllers.store)
 
    /*EDITAR PRODUCTO*/
  router.get('/editar', productoControllers.edit); 
