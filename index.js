2c995fd4de93f64336668e2f34a210ee3ecf81fc
Peters0n <petersonflemm@gmail.com>
2021-03-07 14:13:07 -0400

    xi index

--- index.js
+++ index.js
@@ -14,11 +14,6 @@ app.use(express.static(publicPath));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false })); //URL encode  - Para que nos pueda llegar la información desde el formulario al req.body
-app.use(methodOverride('_method')); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
-<<<<<<< HEAD
-=======
-
->>>>>>> 9dab4919c5564766462ed9b123cdfe1dab0b7f20
 
 //Requiero el módulo que contiene las rutas (mainRoutes)
 let mainRoutes = require("./routers/mainRoutes.js");
