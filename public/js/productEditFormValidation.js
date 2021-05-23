fbd7624b3f55d270ec3b641e85b464272db7466d
paularassiga <paularassiga@gmail.com>
2021-05-23 17:23:18 -0300

    Arreglo validacion del formulario de edicion + validaciones de imagenes front

--- public/js/productEditFormValidation.js
+++ public/js/productEditFormValidation.js
@@ -1,87 +1,146 @@
 window.addEventListener('load',function(){
+
+    function ValidarImagen(obj){
+        var uploadFile = obj.files[0];
+        
+        if (!window.FileReader) {
+            alert('El navegador no soporta la lectura de archivos');
+            return;
+        }
+    
+        if (!(/\.(jpg|png|jpeg)$/i).test(uploadFile.name)) {
+            alert('El archivo a adjuntar no es una imagen');
+        }
+        else {
+            var img = new Image();
+            img.onload = function () {
+                if (this.width.toFixed(0) != 200 && this.height.toFixed(0) != 200) {
+                    alert('Las medidas deben ser: 200 * 200');
+                }
+                else if (uploadFile.size > 20000)
+                {
+                    alert('El peso de la imagen no puede exceder los 200kb')
+                }
+                else {
+                    alert('Imagen correcta :)')                
+                }
+            };
+            img.src = URL.createObjectURL(uploadFile);
+        }                 
+    }
+
     let formulario = document.querySelector('.edit-form');
 
     formulario.addEventListener('submit', function(e){
 
        let errores = [];
 
-       let campoNombre = document.querySelector('input.form-input-name')
+       let campoNombre = document.querySelector('.form-input-name')
+       console.log(campoNombre)
        if(campoNombre.value.length == 0){
            errores.push('El campo nombre tiene que estar completo');
        }else if(campoNombre.value.length < 5) {
            errores.push('El campo nombre debe contener al menos 5 caracteres')
        }
 
-       let campoResumen = document.querySelector('input.form-input-descr')
+       let campoResumen = document.querySelector('.form-input-text')
        if(campoResumen.value.length == 0){
            errores.push('El campo resumen tiene que estar completo');
        }
 
-       let campoPrecio = document.querySelector('input.form-input-price')
+       let campoPrecio = document.querySelector('.form-input-price')
        if(campoPrecio.value.length == 0){
            errores.push('El campo precio tiene que estar completo');
        }
 
-       let campoStock = document.querySelector('input.form-input-stock')
+       let campoStock = document.querySelector('.form-input-stock')
        if(campoStock.value.length == 0){
            errores.push('El campo stock tiene que estar completo');
        }
 
-       let campoC1 = document.querySelector('input.form-input-characteristic1')
+       let campoC1 = document.querySelector('.form-input-characteristic1')
        if(campoC1.value.length == 0){
            errores.push('El campo característica 1 tiene que estar completo');
        }
 
-       let campoC2 = document.querySelector('input.form-input-characteristic2')
+       let campoC2 = document.querySelector('.form-input-characteristic2')
        if(campoC2.value.length == 0){
            errores.push('El campo característica 2 tiene que estar completo');
        }
 
-       let campoC3 = document.querySelector('input.form-input-characteristic3')
+       let campoC3 = document.querySelector('.form-input-characteristic3')
        if(campoC3.value.length == 0){
            errores.push('El campo característica 3 tiene que estar completo');
        }
 
-       let campoC4 = document.querySelector('input.form-input-characteristic4')
+       let campoC4 = document.querySelector('.form-input-characteristic4')
        if(campoC4.value.length == 0){
            errores.push('El campo característica 4 tiene que estar completo');
        }
 
-       let titleDescription1 = document.querySelector('input.form-input-titleDescription1')
+       let titleDescription1 = document.querySelector('.form-input-titleDescription1')
        if(titleDescription1.value.length == 0){
            errores.push('El campo titulo descripción 1 tiene que estar completo');
        }
 
-       let description1 = document.querySelector('input.form-input-description1')
+       let description1 = document.querySelector('.form-input-description1')
        if(description1.value.length == 0){
            errores.push('El campo descripción 1 tiene que estar completo');
        }else if(description1.value.length < 20) {
            errores.push('El campo descripción 1 debe contener al menos 20 caracteres')
        }
 
-       let titleDescription2 = document.querySelector('input.form-input-titleDescription2')
+       let titleDescription2 = document.querySelector('.form-input-titleDescription2')
        if(titleDescription2.value.length == 0){
            errores.push('El campo titulo descripción 2 tiene que estar completo');
        }
 
-       let description2 = document.querySelector('input.form-input-description2')
+       let description2 = document.querySelector('.form-input-description2')
        if(description2.value.length == 0){
            errores.push('El campo descripción 2 tiene que estar completo');
        }else if(description2.value.length < 20) {
            errores.push('El campo descripción 2 debe contener al menos 20 caracteres')
        }
 
-       let titleDescription3 = document.querySelector('input.form-input-titleDescription3')
+       let titleDescription3 = document.querySelector('.form-input-titleDescription3')
        if(titleDescription3.value.length == 0){
            errores.push('El campo titulo descripción 3 tiene que estar completo');
        }
 
-       let description3 = document.querySelector('input.form-input-description3')
+       let description3 = document.querySelector('.form-input-description3')
        if(description3.value.length == 0){
            errores.push('El campo descripción 3 tiene que estar completo');
        }else if(description3.value.length < 20) {
            errores.push('El campo descripción 3 debe contener al menos 20 caracteres')
        }
+
+       let imagen1 = document.querySelector('.form-input-imagen1')
+       if(imagen1.value.length == 0){
+            errores.push('Por favor subí la imagen 1');
+        } else if (!(/\.(jpg|png|gif|jpeg)$/i).test(imagen1.value)) {
+            errores.push('La imagen 1 tiene un formato invalido');
+       }   
+
+       let imagen2 = document.querySelector('.form-input-imagen2')
+       if(imagen1.value.length == 0){
+            errores.push('Por favor subí la imagen 2');
+        } else if (!(/\.(jpg|png|gif|jpeg)$/i).test(imagen2.value)) {
+            errores.push('La imagen 2 tiene un formato invalido');
+       }   
+
+       let imagen3 = document.querySelector('.form-input-imagen3')
+       if(imagen1.value.length == 0){
+            errores.push('Por favor subí la imagen 2');
+        } else if (!(/\.(jpg|png|gif|jpeg)$/i).test(imagen3.value)) {
+            errores.push('La imagen 3 tiene un formato invalido');
+       }  
+
+       let imagen4 = document.querySelector('.form-input-imagen4')
+       if(imagen1.value.length == 0){
+            errores.push('Por favor subí la imagen 4');
+        } else if (!(/\.(jpg|png|gif|jpeg)$/i).test(imagen4.value)) {
+            errores.push('La imagen 4 tiene un formato invalido');
+       }  
 
        if(errores.length>0){
            e.preventDefault();
